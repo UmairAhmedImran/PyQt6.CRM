e1cdf7a586acd7863a213fd65a76d6ac22a3a043
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-07-13 20:28:50 +0500

    creating a full size window and adding an input field

--- src/home_crm.py
+++ src/home_crm.py
@@ -1,11 +1,14 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLineEdit, QLabel
 
+string = "hello"
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("CRM")
-        
+        self.setWindowTitle("CRM") # adding title to the page
+        self.setGeometry(0,0,1530,800) # adding specific size to the page
+        self.input = QLineEdit(self)
+        self.input.move(80,100)
 
 app = QApplication(sys.argv)
 window = MainWindow()
