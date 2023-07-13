3f65130f0f1b1ead371785fa128eea1e40e1efed
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-07-13 21:38:48 +0500

    Creating a simple app which can take input

--- src/home_crm.py
+++ src/home_crm.py
@@ -1,16 +1,37 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLineEdit, QLabel
+from PyQt6.QtWidgets import QApplication, QWidget,QLineEdit, QVBoxLayout, QPushButton, QLabel
+from PyQt6.QtCore import Qt
 
 string = "hello"
-class MainWindow(QMainWindow):
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("CRM") # adding title to the page
-        self.setGeometry(0,0,1530,800) # adding specific size to the page
-        self.input = QLineEdit(self)
-        self.input.move(80,100)
+        self.resize(250,250) # adding specific size to the page
+
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        text = QLabel("Enter text here")
+        layout.addWidget(text)
+
+        self.input = QLineEdit()
+        self.input.setDragEnabled(False)
+        layout.addWidget(self.input, alignment= Qt.AlignmentFlag.AlignCenter)
+
+        button = QPushButton("Get Text")
+        button.clicked.connect(self.get)
+        layout.addWidget(button)
+
+        button = QPushButton("Clear Text")
+        button.clicked.connect(self.input.clear)
+        layout.addWidget(button)
+
+    def get(self):
+        text = self.input.text()
+        print(text)
 
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
-app.exec()
+sys.exit(app.exec())
