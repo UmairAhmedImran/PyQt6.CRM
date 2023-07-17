ce6cc40e1055415f46e28dc860d66a8e168bf1d8
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-07-17 22:47:14 +0500

    Creating a function to call multiple labels

--- src/login.py
+++ src/login.py
@@ -8,9 +8,18 @@ class Window(QWidget):
         self.resize(500, 400)
         self.setWindowTitle("CodersLegacy")
 
-        label = QLabel("Username :", self)
-        label.setFont(QFont('Arial', 10))
-        label.move(80, 80)
+        self.labels("Username :", 'Arial', 10, 80, 80)
+
+        self.labels("Password :", 'Arial', 10, 80, 180)
+
+    def labels(self, text:str, font_family:str, size:int, x:int, y: int):
+        label = QLabel(text, self)
+        label.setFont(QFont(font_family, size))
+        label.move(x, y)
+
+        # label = QLabel("Username :", self)
+        # label.setFont(QFont('Arial', 10))
+        # label.move(80, 80)
 
 
 app = QApplication(sys.argv)
