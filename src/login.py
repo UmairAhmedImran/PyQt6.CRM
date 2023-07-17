88ad13141ff73a0b2e7324136759aeaa43e96ab0
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-07-17 23:38:31 +0500

    Creating functions to simplify the login window

--- src/login.py
+++ src/login.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import  QApplication, QWidget, QLabel
+from PyQt6.QtWidgets import  QApplication, QLineEdit,QWidget, QLabel
 from PyQt6.QtGui import QFont
 import sys
 
@@ -8,18 +8,22 @@ class Window(QWidget):
         self.resize(500, 400)
         self.setWindowTitle("CodersLegacy")
 
-        self.labels("Username :", 'Arial', 10, 80, 80)
+        self.labels("Username :", 'Times New Roman', 10, 80, 80)
+        self.fields(80, 110, QLineEdit.EchoMode.Normal)
 
-        self.labels("Password :", 'Arial', 10, 80, 180)
+        self.labels("Password :", 'Times New Roman', 10, 80, 180)
+        self.fields(80, 210, QLineEdit.EchoMode.Password)
 
     def labels(self, text:str, font_family:str, size:int, x:int, y: int):
         label = QLabel(text, self)
         label.setFont(QFont(font_family, size))
         label.move(x, y)
 
-        # label = QLabel("Username :", self)
-        # label.setFont(QFont('Arial', 10))
-        # label.move(80, 80)
+    def fields(self, x:int, y:int, Echomode):
+        self.input = QLineEdit(self)
+        self.input.setEchoMode(Echomode)
+        self.input.setDragEnabled(False)
+        self.input.move(x, y)
 
 
 app = QApplication(sys.argv)
