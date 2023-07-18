7fa08ed154e89e0eb58a27658bf595c9450f7d33
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-07-18 22:07:40 +0500

    Adding an image to the login form

--- src/login.py
+++ src/login.py
@@ -1,5 +1,6 @@
-from PyQt6.QtWidgets import  QApplication, QLineEdit,QWidget, QLabel
-from PyQt6.QtGui import QFont
+from PyQt6.QtWidgets import  QApplication, QLineEdit,QWidget, QLabel, QVBoxLayout
+from PyQt6.QtGui import QFont, QPixmap
+from PyQt6.QtCore import Qt
 import sys
 
 class Window(QWidget):
@@ -8,11 +9,36 @@ class Window(QWidget):
         self.resize(500, 400)
         self.setWindowTitle("CodersLegacy")
 
-        self.labels("Username :", 'Times New Roman', 10, 80, 80)
-        self.fields(80, 110, QLineEdit.EchoMode.Normal)
+        self.labels("Username :", 'Times New Roman', 10, 60, 90)
+        self.fields(60, 120, QLineEdit.EchoMode.Normal)
 
-        self.labels("Password :", 'Times New Roman', 10, 80, 180)
-        self.fields(80, 210, QLineEdit.EchoMode.Password)
+        self.labels("Password :", 'Times New Roman', 10, 60, 180)
+        self.fields(60, 210, QLineEdit.EchoMode.Password)
+
+        self.images(300, 300, "login_image.png")
+
+    def images(self, new_width:int, new_height:int, image_url:str):
+
+        self.label = QLabel(self)
+
+        # Load the image using QPixmap
+        pixmap = QPixmap(image_url)
+
+        scaled_pixmap = pixmap.scaled(new_width, new_height)
+        
+
+        # Set the pixmap as the content of the QLabel
+        self.label.setPixmap(scaled_pixmap)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.label)
+
+        # Set the alignment of the label within the layout (center)
+        layout.setAlignment(Qt.AlignmentFlag.AlignRight)
+
+        # Set the layout as the main layout for the window
+        self.setLayout(layout)
+
 
     def labels(self, text:str, font_family:str, size:int, x:int, y: int):
         label = QLabel(text, self)
@@ -21,6 +47,7 @@ class Window(QWidget):
 
     def fields(self, x:int, y:int, Echomode):
         self.input = QLineEdit(self)
+        self.input.setGeometry(x, y, 150, 25)
         self.input.setEchoMode(Echomode)
         self.input.setDragEnabled(False)
         self.input.move(x, y)
