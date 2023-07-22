5a9090c4c8780579e329b5bec10e3d311836d27f
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-07-22 19:55:18 +0500

    Adding buttons to the dashboard class

--- src/login.py
+++ src/login.py
@@ -3,7 +3,7 @@ from PyQt6.QtGui import QFont, QPixmap
 from PyQt6.QtCore import Qt
 import sys
 
-text = []
+credentials = []
 class Window(QWidget):
     def __init__(self):
         super().__init__()
@@ -15,23 +15,24 @@ class Window(QWidget):
 
         self.labels("Password :", 'Times New Roman', 10, 40, 140)
         self.fields(40, 170, QLineEdit.EchoMode.Password)
-
+        
         self.images(300, 300, "login_image.png")
 
-        self.buttons(40, 240, 130, 35, "Login", self.passing)
+        self.buttons(40, 240, 130, 35, "Login", self.going_to_dashboard, "background-color: black; color: white;")
 
-    def buttons(self, w: int, x: int, y: int, z: int, text: str, fn):
+    def buttons(self, w: int, x: int, y: int, z: int, text: str, fn, css:str):
          self.button = QPushButton(text, self)
          self.button.setFocus()
          self.button.setGeometry(w, x, y, z)
          self.button.clicked.connect(fn)
          self.button.setCursor(Qt.CursorShape.PointingHandCursor)
-         self.button.setStyleSheet("background-color: black; color: white;")
+         self.button.setStyleSheet(css)
 
-    def passing(self):
-        self.close()
-        self.w = Dashboard()
-        self.w.show()
+    def going_to_dashboard(self):
+            self.close()
+            self.w = Dashboard()
+            self.w.show()
+            print(credentials)
 
     def images(self, new_width:int, new_height:int, image_url:str):
 
@@ -58,6 +59,7 @@ class Window(QWidget):
         self.input.setEchoMode(Echomode)
         self.input.setDragEnabled(False)
         self.input.move(x, y)
+        self.input.text()
 
 class Dashboard(QWidget):
     def __init__(self):
@@ -67,6 +69,18 @@ class Dashboard(QWidget):
 
         Window.images(self, 300, 320, 'i_health_care_center.jpg')
 
+        Window.buttons(self, 30, 50, 130, 30, "Appointment", self.passing, "background-color: #090; color: white;")
+
+        Window.buttons(self, 30, 100, 130, 30, "Cancel Appointment", self.passing, "background-color: #090; color: white;")
+
+        Window.buttons(self, 30, 150, 130, 30, "Change Appointment", self.passing, "background-color: #090; color: white;")
+
+        Window.buttons(self, 30, 200, 130, 30, "view Appointment", self.passing, "background-color: #090; color: white;")
+
+        Window.buttons(self, 30, 250, 130, 30, "Logout", self.passing, "background-color: #090; color: white;")
+
+    def passing(self):
+        pass
 
 app = QApplication(sys.argv)
 window = Window()
